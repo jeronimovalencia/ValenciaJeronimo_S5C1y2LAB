ca97b975e7831acceecc942f29935a5e23ad74b2
Jeronimo Valencia <j.valencia11@uniandes.edu.co>
2018-07-05 15:07:48 -0500

    mejora de parámetros a variar en el filtrado de la imagen

--- filtro_ruido.py
+++ filtro_ruido.py
@@ -12,6 +12,7 @@ xdim = np.shape(imagen_FT)[0]
 ydim = np.shape(imagen_FT)[1]
 
 M = 50
+K = 50
 
 imagenCentro_FT = np.copy(imagen_FT)
 imagenCentro_FT[:xdim/2 -M,:] = 0
@@ -20,27 +21,27 @@ imagenCentro_FT[:,:ydim/2 -M] = 0
 imagenCentro_FT[:,ydim/2 +M:] = 0
 
 imagenBorde_FT = np.copy(imagen_FT)
-imagenBorde_FT[xdim/2 -200:xdim/2 +200, ydim/2 -300:ydim/2 +300] = 0
+imagenBorde_FT[K:-K, K:-K] = 0
 
 #Graficamos la imagen con su respectiva trasnformada de Fourier
 plt.figure()
-plt.subplot(311)
+plt.subplot(211)
 plt.imshow(np.real(imagen), cmap='gray')
-plt.subplot(312)
+plt.subplot(212)
 plt.imshow(np.real(imagen_FT), cmap='gray', vmin = 0, vmax = 200)
-plt.subplot(313)
-plt.imshow(np.real(imagenBorde_FT), cmap='gray', vmin = 0, vmax = 200)
-plt.show() 
+#plt.show() 
 plt.clf()
 plt.close()
 
 
-lugaresPicos = np.where(np.abs(imagen_FT - imagenCentro_FT) > 120)
+lugaresPicos = np.where(np.abs(imagen_FT - imagenCentro_FT) > 130)
 imagen_FT[lugaresPicos]=0
 imagen_FT[xdim/2 -M:xdim/2 +M, ydim/2 -M:ydim/2 +M] = 0
+imagen_FT[:K,:]=0
+imagen_FT[-K:,:]=0
+imagen_FT[:,:K] = 0
+imagen_FT[:,-K:] = 0
 
 
-plt.figure()
-plt.subplot(211)
-plt.imshow(np.abs(ifft2(imagen_FT + imagenCentro_FT)), cmap='gray')
+plt.imshow(np.abs(ifft2(imagen_FT + imagenCentro_FT + imagenBorde_FT)), cmap='gray')
 plt.show()
